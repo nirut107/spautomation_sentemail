e7767fca58e08ea51a9b62360b9cbe552293ad5d
nirut <atipanyo107@gmail.com>
2026-01-20 01:09:35 +0700

    add loga

--- SpAutomation/app/api/send-email/route.ts
+++ SpAutomation/app/api/send-email/route.ts
@@ -78,7 +78,7 @@ export async function POST(req: NextRequest) {
     attachments: [
       {
         filename: "logo.png",
-        path: "./logo.png",
+        path: "/public/logo.png",
         cid: "logo",
       },
       ...attachments,
